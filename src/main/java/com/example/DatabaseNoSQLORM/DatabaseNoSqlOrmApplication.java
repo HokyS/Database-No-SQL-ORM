7867ff763fdc31b3925eaf22705ade0c4f943a4d
Mario Cotumaccio <mariocotumaccio1@gmail.com>
2022-10-22 13:47:51 +0200

    Creation of MongoDBConfig

---
+++ src/main/java/com/example/DatabaseNoSQLORM/DatabaseNoSqlOrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.DatabaseNoSQLORM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseNoSqlOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseNoSqlOrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DatabaseNoSQLORM/DatabaseNoSqlOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DatabaseNoSQLORM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseNoSqlOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
